6c77b98d8e16601e7717f8db9370e2f093b4935c
indrajau <119101363+indrajau@users.noreply.github.com>
2023-07-01 12:25:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import Navbar from './Components/Navbar';
+import ProductList from './Components/ProductList';
+import Cart from './Components/Cart';
+import BodyText from './Components/BodyText';
+import BodyButton from './Components/BodyButton';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <BodyText/>
+      <ProductList />
+      <Cart/>
+      <BodyButton/>
+      <Footer/>
+
+    </>
+      );
+    }
+    
+    export default App;
